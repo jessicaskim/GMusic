d4a3fd22c22e2848feb25b073fa133f3708c6f6b
David Dlima <dlima.david@gmail.com>
2018-10-12 22:27:54 -0400

    added .env & youtube search

--- GMusicApp/routes/queue_controller.js
+++ GMusicApp/routes/queue_controller.js
@@ -1,4 +1,15 @@
-const Express = require('express');
-const Router  = Express.Router();
+require('dotenv').config({ silent: true });
+
+const Express           = require('express');
+const Router            = Express.Router();
+const youtubeSearch     = require('youtube-api-v3-search');
+
+Router.get('/', (req, res) => {
+    // res.send("GET songs route is working")
+    youtubeSearch(process.env.YOUTUBE_API_KEY,{q:"honey kehlani"})
+        .then(data => {
+            res.send(data);
+        })
+})
 
 module.exports = Router;
