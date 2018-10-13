1d495192943015dc2c4ecdd0eb39cff83ccb6cda
David Dlima <dlima.david@gmail.com>
2018-10-12 23:28:54 -0400

    re-fixed merge

--- GMusicApp/routes/queue_controller.js
+++ GMusicApp/routes/queue_controller.js
@@ -5,16 +5,16 @@ const Router            = Express.Router();
 const youtubeSearch     = require('youtube-api-v3-search');
 
 Router.get('/', (req, res) => {
-    // res.send("GET songs route is working")
-    youtubeSearch(process.env.YOUTUBE_API_KEY,{q:"honey kehlani"})
-        .then(videos => {
-            // res.send(videos);
-            res.render('queue/index',{videos});
-        })
+    res.send("GET songs route is working")
+
 })
 
 Router.get('/suggest', (req, res) => {
-    res.render('queue/suggest');
+    youtubeSearch(process.env.YOUTUBE_API_KEY,{q:"honey kehlani"})
+    .then(videos => {
+        // res.send(videos);
+        res.render('queue/suggest',{videos});
+    })
 })
 
 module.exports = Router;
