846d00cf8323be7d0f96ff8155f205dbe74664ce
David Dlima <dlima.david@gmail.com>
2018-10-12 20:12:13 -0400

    setup db

--- GMusicApp/app.js
+++ GMusicApp/app.js
@@ -1,15 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-const expressLayouts = require('express-ejs-layouts');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError     = require('http-errors');
+const express         = require('express');
+const expressLayouts  = require('express-ejs-layouts');
+const path            = require('path');
+const cookieParser    = require('cookie-parser');
+const logger          = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 const queueRouter = require('./routes/queue_controller');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
